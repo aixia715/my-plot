ba8847e50b0f139780be84409bba7edd8674c706
aixia715 <aixia715@163.com>
2022-09-28 00:14:14 +0800

    完善绘制图的代码

--- plot_csv.py
+++ plot_csv.py
@@ -6,28 +6,34 @@
 
 from data_csv import DataCsv
 import typing
-
+import matplotlib.cm as cm
+import numpy as np
 import matplotlib.pyplot as plt
 
 
 class PlotCSV(object):
     def __init__(self):
         self.csv_list: typing.List[DataCsv] = []
+        self.plot_param = None
         pass
 
     def add_csv(self, csv_obj: DataCsv):
         self.csv_list.append(csv_obj)
 
-    def plot(self, title, xlabel, ylabel):
-        param = []
-        for csv_obj in self.csv_list:
-            param.append(csv_obj.x)
-            param.append(csv_obj.y)
+    def set_plot_param(self, title, xlabel, ylabel, grid=False, figsize=None):
+        self.plot_param = {'title': title, 'xlabel': xlabel, 'ylabel': ylabel, 'grid': grid, 'figsize': figsize}
 
-        plt.plot(*param)
-        plt.title(title, fontsize=12)
-        plt.xlabel(xlabel)
-        plt.ylabel(ylabel)
+    def semilogx(self):
+        # 绘制x坐标为对数的图
+        colors = cm.rainbow(np.linspace(0, 1, len(self.csv_list)))
+        assert self.plot_param is not None
+        plt.figure(figsize=self.plot_param['figsize'])  # 新建图
+        plt.grid(self.plot_param['grid'])
+        plt.title(self.plot_param['title'], fontsize=12)
+        plt.xlabel(self.plot_param['xlabel'])
+        plt.ylabel(self.plot_param['ylabel'])
+        for i, csv_obj in enumerate(self.csv_list):
+            plt.semilogx(csv_obj.x, csv_obj.y, color=colors[i])
 
         plt.show()
 
@@ -36,4 +42,11 @@ if __name__ == "__main__":
     csv_obj = DataCsv(path='test_data/test.csv')
     plot = PlotCSV()
     plot.add_csv(csv_obj)
+    plot.set_plot_param(
+        title='Test',
+        xlabel='freq',
+        ylabel='phase noise',
+        grid=True,
+        figsize=(12, 6)
+    )
     plot.plot('Test', 'freq', 'phase noise')
